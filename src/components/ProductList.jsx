4cdbda5e615364d0afddc6c5a62c586da3e56cc4
Joshua.Vazquez@igrtec.com <Joshua.Vazquez@igrtec.com>
2023-04-03 00:27:51 -0600

    update

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,30 +3,9 @@ import useGetProduct from "../hooks/useGetProduct";
 import ProductItem from "./ProductItem";
 import '@styles/ProductList.scss';
 const ProductList = () => {
-  const axios = require('axios');
-  
-  let data = JSON.stringify({
-    "numPag": 1
-  });
-  
-  let config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: 'https://keen-napier.68-168-208-58.plesk.page/api/ProductoGral/GetProductos',
-    headers: { 
-      'Content-Type': 'application/json'
-    },
-    data : data
-  };
-  
-  axios.request(config)
-  .then((response) => {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-  
+    const API = "https://fakestoreapi.com/products";
+
+    const data = useGetProduct(API)
 
   return (
     <section className="main-container">
